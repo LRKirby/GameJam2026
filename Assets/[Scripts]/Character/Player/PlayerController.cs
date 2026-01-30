9b2a6ed4b4b30ad328e3f91a3e1b4623a117f3c1
Sam Burke <maxandsamrock@gmail.com>
2026-01-30 16:42:39 -0500

    created base character files

---
+++ Assets/[Scripts]/Character/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public void Awake() {
+
+    }
+
+    public void Update() {
+
+    }
+
+    public void OnMove(CallbackContext context) {
+
+    }
+
+    public void OnDeath() {
+
+    }
+
+    public void TakeDamage() {
+
+    }
+}
+
---
+++ Assets/[Scripts]/Core/Character.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class Character : MonoBehaviour
+{
+	private int maxHealth;
+	private int currentHealth;
+	private float moveSpeed;
+	private float maxSpeed;
+	SpriteRenderer sr;
+	Animator anim;
+
+	protected int MaxHealth {
+		get {
+			return maxHealth;
+		}
+		set {
+			maxHealth = Math.Abs(value);
+		}
+	}
+
+	protected int CurrentHealth {
+		get {
+			return currentHealth;
+		}
+		set {
+			currentHealth = Math.Clamp(value, 0, maxHealth);
+		}
+	}
+
+	protected float MaxSpeed {
+		get  {
+			return maxSpeed;
+		}
+		set {
+			maxSpeed = Math.Abs(value);
+		}
+	}
+
+	protected float MoveSpeed {
+		get {
+			return moveSpeed;
+		}
+		set {
+			moveSpeed = Math.Clamp(value, 0, maxSpeed);
+		}
+
+	}
+
+	protected virtual void Awake() {
+        Debug.Log("Awake in Character.cs");
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        sr.sortingLayerName = "Characters";
+        currentHealth = maxHealth;
+    }
+}
