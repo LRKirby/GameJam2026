aa8eb18f8d0db8670dd32dd1863379d2b7040cf8
MarshallAmaral <marshallamaral27@gmail.com>
2026-01-31 12:01:04 -0500

    Added battery and jumpscare image

---
+++ Assets/[Scripts]/BatteryUI.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryUI : MonoBehaviour
+{
+    public GameObject FlashLight;
+
+
+    public float batteryLife = 5f;
+
+    public Image battery_100;
+    public Image battery_75;
+    public Image battery_50;
+    public Image battery_25;
+    public Image battery_0;
+
+
+    private float currentBatteryLife;
+
+    private void Start()
+    {
+        currentBatteryLife = batteryLife;
+        UpdateBatteryUI();
+    }
+
+
+    private void Update()
+    {
+        currentBatteryLife -= Time.deltaTime;
+        if (currentBatteryLife <= 0)
+        {
+            currentBatteryLife = 0;
+        }
+        UpdateBatteryUI();
+    }
+
+    private void UpdateBatteryUI()
+    {
+        float batteryPercentage = (currentBatteryLife / batteryLife) * 100f;
+        battery_100.enabled = batteryPercentage > 75f;
+        battery_75.enabled = batteryPercentage <= 75f && batteryPercentage > 50f;
+        battery_50.enabled = batteryPercentage <= 50f && batteryPercentage > 25f;
+        battery_25.enabled = batteryPercentage <= 25f && batteryPercentage > 0f;
+        battery_0.enabled = batteryPercentage <= 0f;
+
+       
+        if (batteryPercentage <= 0f)
+        {
+            FlashLight.SetActive(false);
+        }
+
+    }
+   
+}
+
+
+
